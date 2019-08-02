982b12814d39a0cf01e710e243ee6c9d785505a7
frosty <aaronfrost@gmail.com>
2019-08-02 10:15:46 -0600

    Remove API keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,3 +5,15 @@ export const environment = {
         PRIVATE_KEY: 'INSERT YOUR KEY FIRST',
     },
 };
+
+if (environment.MARVEL_API.PUBLIC_KEY === 'INSERT YOUR KEY FIRST') {
+    /**
+     * To get access to the marvel API, you need to go to their site and sign up for an account.
+     * Go Here: https://developer.marvel.com/
+     *
+     * Once you have done that, in their portal, you will need to add http://localhost to their
+     * whitelisted domains. If you don't do this, it will fail for you.
+     */
+    document.write('INSERT YOUR KEY FIRST');
+    throw new Error('You must setup a public and private API key first.');
+}
